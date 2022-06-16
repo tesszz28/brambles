eba53cbd50aab61c7b7661d44f723f16e884ca80
tesszz28 <viewa28@gmail.com>
2022-06-16 16:35:51 +1000

    page1

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,15 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import rhino3dm import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Danae from UNSW CODE!'
+
+@app.route('/page1')
+def hello_page1():
+    return 'Welcome to my first non-home page, fully coded by yours truly!'
 
