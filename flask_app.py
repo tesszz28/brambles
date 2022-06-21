58c59c8180c2783fd85349cc1b58f4ce34324a3f
tesszz28 <viewa28@gmail.com>
2022-06-21 20:53:56 +1000

    excelvibes

--- flask_app.py
+++ flask_app.py
@@ -3,14 +3,17 @@
 
 from flask import Flask
 from rhino3dm import *
+from openpyxl import load_workbook
+wb = load_workbook(filename = 'Stats IC Workbook.xlsx')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Danae from UNSW CODE!'
+def sheetname_display():
+    return print(wb.sheetnames)
 
-@app.route('/page1')
-def hello_page1():
-    return 'Welcome to my first non-home page, fully coded by yours truly!'
-
+#@app.route('/page1')
+#def hello_page1():
+#    return 'Welcome to my first non-home page, fully coded by yours truly!'
