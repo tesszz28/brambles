90b1bb7380695d874018427690e5c217e4d08a7d
tesszz28 <viewa28@gmail.com>
2022-06-21 22:00:35 +1000

    excel2

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,15 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
-from rhino3dm import *
+#from rhino3dm import *
 from openpyxl import load_workbook
-wb = load_workbook(filename = 'Stats IC Workbook.xlsx')
+wb = load_workbook('C:/Users/danae/Documents/Studies and Whatever Folder/Uni Years/Year 3/T2 2022/CODE2120/Stats IC Workbook.xlsx')
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Danae from UNSW CODE!'
+#def hello_world():
+#    return 'Hello from Danae from UNSW CODE!'
 def sheetname_display():
     return print(wb.sheetnames)
 
